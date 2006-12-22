8b9635bb520822d1c67befa347b6fa99d8664ab9
Moritz Onken <onken@netcubed.de>
2006-12-22 14:47:01 +0000

    Import of PERLER/DBIx-Class-SaltedPasswords-0.02 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-SaltedPasswords
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLER/DBIx-Class-SaltedPasswords-0.02.tar.gz
    gitpan-cpan-author:       PERLER
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/SaltedPasswords.pm
+++ lib/DBIx/Class/SaltedPasswords.pm
@@ -3,43 +3,69 @@ package DBIx::Class::SaltedPasswords;
 use base qw(DBIx::Class);
 use strict;
 use warnings;
+use Digest::MD5 qw( md5_hex );
 
-require Exporter;
+our $VERSION = '0.02';
 
-
-our $VERSION = '0.01';
-
-
-
-
-
-
-__PACKAGE__->mk_classdata( 'salted_enabled' => 1 );
-__PACKAGE__->mk_classdata( 'salted_columns' => [] );
-__PACKAGE__->mk_classdata( 'salted_salt' => "perlrulez" );
-
+__PACKAGE__->mk_classdata( 'salted_enabled'     => 1 );
+__PACKAGE__->mk_classdata( 'salted_column'      => "" );
+__PACKAGE__->mk_classdata( 'salted_salt_length' => 6 );
+__PACKAGE__->mk_classdata( 'salted_salt_column' => 'salt' );
 
 sub saltedpasswords {
 	my $self = shift;
 	my %args = @_;
-	$args{columns} = [ $args{columns} ]
-	  unless ( ref( $args{columns} ) eq "ARRAY" );
-	$self->salted_columns($args{columns});
-	$self->salted_enabled($args{enabled}) if exists $args{enabled};
-	$self->salted_salt($args{salt}) if exists $args{salt};
+	$self->salted_column( $args{column} );
+	$self->salted_enabled( $args{enabled} ) if exists $args{enabled};
+	$self->salted_salt_length( $args{salt_length} )
+	  if exists $args{salt_length};
+	$self->salted_salt_column( $args{salt_column} )
+	  if exists $args{salt_column};
+
 }
 
 sub insert {
 	my $self = shift;
 	if ( $self->salted_enabled ) {
-		for my $column ( @{ $self->salted_columns } ) {
-			#die $self->get_column($column) . $self->salted_salt;
-			$self->set_column( $column,
-				$self->get_column($column) . $self->salted_salt )
-			  if defined $self->get_column($column);
+		my $salt;
+		$salt .= ( 'a' .. 'z', 'A' .. 'Z', 0 .. 9 )[ int( rand() * 62 ) ]
+		  for ( 1 .. $self->salted_salt_length );
+		if ( defined $self->get_column( $self->salted_column ) ) {
+			$self->set_column( $self->salted_column,
+				md5_hex( $self->get_column( $self->salted_column ) . $salt ) );
+			$self->set_column( $self->salted_salt_column, $salt );
+
 		}
 	}
 	return $self->next::method(@_);
+}
+## copy of insert
+sub update {
+	my $self = shift;
+	if ( $self->salted_enabled ) {
+		my $salt;
+		$salt .= ( 'a' .. 'z', 'A' .. 'Z', 0 .. 9 )[ int( rand() * 62 ) ]
+		  for ( 1 .. $self->salted_salt_length );
+		if ( defined $self->get_column( $self->salted_column ) ) {
+			$self->set_column( $self->salted_column,
+				md5_hex( $self->get_column( $self->salted_column ) . $salt ) );
+			$self->set_column( $self->salted_salt_column, $salt );
+
+		}
+	}
+	return $self->next::method(@_);
+}
+
+sub verify_password {
+	my $self = shift;
+	return 0 unless ( $_[0] );
+	die "There is no salt element"
+	  unless ( defined $self->get_column( $self->salted_salt_column ) );
+	die "There is no password element"
+	  unless ( defined $self->get_column( $self->salted_column ) );
+	return md5_hex( $_[0] . $self->get_column( $self->salted_salt_column ) ) eq
+	  $self->get_column( $self->salted_column ) ? 1 : 0;
+
 }
 
 1;
@@ -54,14 +80,54 @@ DBIx::Class::SaltedPasswords - Salts password columns
 
   __PACKAGE__->load_components(qw/SaltedPasswords ... Core /);
   __PACKAGE__->saltedpasswords( 
-     columns => [qw/ password /], 
-     salt    => "perlrulez",
-     enabled => 1
+     column      => "password", # no defaul value
+     salt_lenght => 6,          # default: 6
+     enabled     => 1,          # default: 1
+     salt_column =>'salt'       # default: 'salt'
   );
 
 =head1 DESCRIPTION
 
-Adds a salt to the value for selected columns.
+This module generates for every insert or update of a specified column a random salt, adds it to the value and hashes the completet string with MD5.
+The salt is stored in the salt_column column.
+To verify a password against the table use the verify_password method.
+
+=head1 EXAMPLE
+
+In your table scheme (e.g. User):
+
+  __PACKAGE__->load_components(qw/SaltedPasswords ... Core /);
+  __PACKAGE__->saltedpasswords( 
+     column      => "password"
+  );
+  
+In your application:
+
+  sub register {
+  	$db->resultset('User')->create(
+		{
+			name         => 'Paul',
+			password     => 'secret'
+		}
+	)->update;
+  }
+
+This registers a new user with a crypted password ($password is plaintext, the encryption is done by this module). Make sure the salt column exists.
+
+  my $rs = $db->resultset('User')->search({name => 'Paul'})->first; # Or use find() and your primary key
+  $rs->verify_password('secret');                                   # return 1 if the password is right
+
+This validates the password
+
+=head1 NEW METHODS
+
+The following methods are new:-
+
+=over 4
+
+=item verify_password
+
+return 1 if the password is right, 0 else.
 
 =head1 EXTENDED METHODS
 
@@ -71,14 +137,13 @@ The following L<DBIx::Class::Row> methods are extended by this module:-
 
 =item insert
 
-=head1 TODO
-
-extend methods: update, search, find
+=item update
 
 
 =head1 SEE ALSO
 
 L<DBIx::Class>
+L<DBIx::Class::ColumnsDigest>
 
 =head1 AUTHOR
 
@@ -86,7 +151,7 @@ Moritz Onken (perler)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006 by root
+Copyright (C) 2006 by perler
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
